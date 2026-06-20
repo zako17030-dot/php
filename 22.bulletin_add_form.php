119da59ed66cd7d0c6f9e36d6d7819c90c5bd0dd
HUANGGUANBO <zako17030@gmail.com>
2026-06-20 22:15:20 +0800

    22.bulletin_add_form.php

---
+++ 22.bulletin_add_form.php
@@ -0,0 +1,28 @@
+<?php
+    error_reporting(0);
+    // 啟動 Session
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "please login first";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+      // 若已登入，輸出新增佈告的表單 HTML
+        echo "
+        <html>
+            <head><title>新增佈告</title></head>
+            <body>
+                <form method=post action=23.bulletin_add.php>
+                    標    題：<input type=text name=title><br>
+                    內    容：<br><textarea name=content rows=20 cols=20></textarea><br>
+                    佈告類型：<input type=radio name=type value=1>系上公告 
+                            <input type=radio name=type value=2>獲獎資訊
+                            <input type=radio name=type value=3>徵才資訊<br>
+                    發布時間：<input type=date name=time><p></p>
+                    <input type=submit value=新增佈告> <input type=reset value=清除>
+                </form>
+            </body>
+        </html>
+        ";
+    }
+?>
